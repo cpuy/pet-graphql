abfe4e369886a5dfa8aaf5f46fcd4cf1d93200e7
cpuy <colin.puy@bonitasoft.com>
2017-03-10 16:46:14 +0100

    switch CaseRepo to bonita engine client

--- src/main/java/colin/repository/CaseRepository.java
+++ src/main/java/colin/repository/CaseRepository.java
@@ -1,26 +1,23 @@
 package colin.repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import colin.domain.Case;
+import org.bonitasoft.engine.api.APIClient;
+import org.bonitasoft.engine.bpm.process.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CaseRepository {
 
-    private static Map<Long, Case> store = new HashMap<>();
+    @Autowired
+    private APIClient apiClient;
 
-    static {
-        store.put(1L, new Case(1, "case 1"));
-        store.put(2L, new Case(2, "case 2"));
-        store.put(3L, new Case(3, "case 3"));
-        store.put(4L, new Case(4, "case 4"));
-        store.put(5L, new Case(5, "case 5"));
-        store.put(6L, new Case(6, "case 6"));
-    }
-
-    public Case get(long id) {
-        return store.get(id);
+    public ProcessInstance get(long id)  {
+        try {
+            apiClient.login("install", "install");
+            return apiClient.getProcessAPI().getProcessInstance(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+        return null;
     }
 }
